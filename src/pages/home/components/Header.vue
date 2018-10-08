242f96dd985d1e3aac7dcd48c718f19f6ad4b3e3
xiewjsir <183778760@qq.com>
2018-10-08 18:04:48 +0800

    init project

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市/景点/游玩主题
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeHeader"
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .header
+        display:flex
+        line-height:$headerHeight
+        background-color:$bgColor
+        color:#fff
+        .header-left
+            width:0.64rem
+            float:left
+            .back-icon
+                text-align:center
+                font-size:0.4rem
+        .header-input
+            flex:1
+            height:.64rem
+            line-height:.64rem
+            margin-top:.12rem
+            margin-left:.2rem
+            padding-left:.2rem
+            backgroud-color:#fff
+            border-radius:.1rem
+            color:#ccc
+
+</style>

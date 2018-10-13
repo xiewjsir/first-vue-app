9b1bf1234401b1965161cb644f712b99a7a528a4
xiewjsir <183778760@qq.com>
2018-10-13 22:48:45 +0800

    轮播图

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -37,7 +37,7 @@ module.exports = {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
       'styles': resolve('src/assets/styles'),
-      'common': resolve('src/ommon'),
+      'common': resolve('src/common'),
     }
   },
   module: {

3ab550755e3e1d50e8b146aa18914c4f8089c4d2
xiewjsir <183778760@qq.com>
2018-10-15 17:00:50 +0800

    Revert "轮播图"
    This reverts commit 9b1bf1234401b1965161cb644f712b99a7a528a4.

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,19 +1,16 @@
 <template>
     <div>
         <home-header></home-header>
-        <home-swiper></home-swiper>
     </div>
 </template>
 
 <script>
     import HomeHeader from './components/Header'
-    import HomeSwiper from './components/Swiper'
 
     export default {
         name: "Home",
         components:{
-            HomeHeader,
-            HomeSwiper,
+            HomeHeader
         }
     }
 </script>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,6 @@
             <span class="iconfont">&#xe632;</span>
             输入城市/景点/游玩主题
         </div>
-        <div class="header-right">
-            城市
-            <span class="iconfont arrow-icon">&#xe64a</span>
-        </div>
     </div>
 </template>
 
@@ -40,16 +36,8 @@
             margin-top:.12rem
             margin-left:.2rem
             padding-left:.2rem
-            background-color:#fff
+            backgroud-color:#fff
             border-radius:.1rem
             color:#ccc
-        .header-right
-            min-width: 1.04rem
-            padding: 0 .1rem
-            float: right
-            text-align: center
-            color: #fff
-            .arrow-icon
-                margin-left: -.04rem
-                font-size:.24rem
+
 </style>
